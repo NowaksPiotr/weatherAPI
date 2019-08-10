98a5b116ed6b63364be81ab8bd9f214d7a929e47
NowaksPiotr <nowak.spiotr@gmail.com>
2019-08-10 16:09:57 +0200

    Commits

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let key = '174be33436b5ac8a0340d573ab8f6d96';
 let check_btn = document.getElementById('check_btn');
 let url = 'https://api.openweathermap.org/data/2.5/forecast?q=Poznan&lang=pl&cnt=5&units=metric&appid=' + key; // test url
+
+
 check_btn.onclick = function(){
 
 console.log("test fetcha");

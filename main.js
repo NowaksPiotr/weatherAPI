18e2ae565df96087837abba7dbf3d5d8df408bb3
NowaksPiotr <nowak.spiotr@gmail.com>
2019-08-10 16:22:01 +0200

    Creating fetch and stuff

--- main.js
+++ main.js
@@ -6,5 +6,13 @@ let url = 'https://api.openweathermap.org/data/2.5/forecast?q=Poznan&lang=pl&cnt
 check_btn.onclick = function(){
 
 console.log("test fetcha");
-// fetch(url);
+fetch(url).then(response => {
+    return response.json();
+})
+.then(response => {
+    console.log(response);
+})
+.catch(error => {
+    console.log('error');
+});
 }
